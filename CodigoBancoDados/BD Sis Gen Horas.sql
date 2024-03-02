2869338fb7e1772241251b23fe0c8b440bd21624
Jhonatan <jhonbblv@gmail.com>
2024-03-02 00:03:22 -0300

    Subindo cod BD

---
+++ CodigoBancoDados/BD Sis Gen Horas.sql
@@ -0,0 +1,19 @@
+use Bd_GerenciaHoras;
+
+create table Controle(
+	idCont int identity(100,1) not null,
+	dia date not null,
+	horaIni time not null,
+	horaFin time not null,
+	primary key (idCont)
+)
+
+create table Projetos(
+	idproj int identity(100,1) not null,
+	notProj varchar(40) not null,
+	inicioProj date not null,
+	finalProj date,
+	idControle int,
+	primary key (idProj),
+	foreign key (idControle) references Controle (idCont)
+)
